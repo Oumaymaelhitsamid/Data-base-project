304bc8e3826b7bf330291ee2b0e0690f83423e3b
Damien Trouche <damien.trouche@grenoble-inp.org>
2021-11-22 16:56:30 +0100

    change local repository

---
+++ src/window/AccesWindow.java
@@ -0,0 +1,54 @@
+package window;
+import javax.swing.*;
+
+import java.awt.event.*;
+
+public class AccesWindow {
+
+    public static void main(String[] args) {
+        final JFrame frame = new JFrame("JPasswogggggrdField Demo");
+
+        JLabel lblUser = new JLabel("User Name:");
+        JTextField tfUser = new JTextField(20);
+        lblUser.setLabelFor(tfUser);
+
+        JLabel lblPassword = new JLabel("Password:");
+        final JPasswordField pfPassword = new JPasswordField(20);
+        lblPassword.setLabelFor(pfPassword);
+
+        JButton btnGet = new JButton("Display Password");
+        btnGet.addActionListener(
+                new ActionListener() {
+
+                    public void actionPerformed(ActionEvent e) {
+                        String password = new String(pfPassword.getPassword());
+                        JOptionPane.showMessageDialog(frame,
+                                "Password is " + password);
+                    }
+                });
+
+        JButton btnLogin = new JButton("Login");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new SpringLayout());
+
+        panel.add(lblUser);
+        panel.add(tfUser);
+        panel.add(lblPassword);
+        panel.add(pfPassword);
+        panel.add(btnLogin);
+        panel.add(btnGet);
+
+        SpringUtilities.makeCompactGrid(panel,
+                3, 2, //rows, cols
+                6, 6, //initX, initY
+                6, 6); //xPad, yPad
+	
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 120);
+        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+        
+       
+    }
+}
