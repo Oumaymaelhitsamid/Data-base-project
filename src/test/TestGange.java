1be4352c75d52f58d49ec0d5a5578059f96ad454
Damien Trouche <damien.trouche@grenoble-inp.org>
2021-11-23 07:41:48 +0100

    few modifications

--- src/test/TestGange.java
+++ src/test/TestGange.java
@@ -1,5 +1,11 @@
 package test;
 
-public class TestGange {
+import window.AccesWindow;
 
+public class TestGange {
+	public static void main(String[] args) {
+		AccesWindow login = new AccesWindow();
+		System.out.println("test");
+		
+	}
 }
