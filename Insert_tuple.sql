2f214de47ecc2105c6b4aa0cda68ce92cd443bf2
Valentin Guiziou <valentin.guiziou@grenoble-inp.org>
2021-12-03 16:17:58 +0000

    Insert data

---
+++ Insert_tuple.sql
@@ -0,0 +1,40 @@
+INSERT INTO PRODUITS VALUES ('1', 'XBOX 360', '250', 'console nulle', 'paspourlinstant', 'Console');
+
+INSERT INTO PRODUITS VALUES ('2', 'Playstation 5', '300', 'trop bien', 'nop', 'Console');
+
+INSERT INTO PRODUITS VALUES ('3', 'Guerre et Paix Partie 1', '12', 'Livre de Tolstoï, partie 1', 'plus tard', 'Roman');
+
+INSERT INTO PRODUITS VALUES ('4', 'Guerre et Paix Partie 2', '12', 'Livre de Tolstoï, partie 2', 'un jour peut être', 'Roman');
+
+INSERT INTO PRODUITS VALUES ('5', 'Tintin au Congo', '15', 'Un peu border', 'si dieu le veut', 'BD');
+
+INSERT INTO CATEGORIES VALUES ('Livre');
+
+INSERT INTO CATEGORIES VALUES ('BD');
+
+INSERT INTO CATEGORIES VALUES ('Roman');
+
+INSERT INTO CATEGORIES VALUES ('Console');
+
+INSERT INTO APOURMERE VALUES ('BD', 'Livre');
+
+INSERT INTO APOURMERE VALUES ('Roman', 'Livre');
+
+INSERT INTO COMPTES VALUES ('1', 'alanfoiré@gmail.com', 'alanfoiré', 'foiré', 'alan', '2 rue Belgrade Belgique');
+
+INSERT INTO COMPTES VALUES ('2', 'loriefice@gmail.com', 'loriefice', 'fice', 'lorie', '2 rue Marcel Pagnol Grenoble');
+
+INSERT INTO COMPTES VALUES ('3', 'ottograf@grenoble-inp.org', 'ottograf', 'graf', 'otto', '7 rue de la Paix Paris');
+
+INSERT INTO UTILISATEURS VALUES ('1');
+
+INSERT INTO UTILISATEURS VALUES ('2');
+
+INSERT INTO UTILISATEURS VALUES ('3');
+
+INSERT INTO CARACTERISTIQUES VALUES ('2', 'poids', '600g');
+
+INSERT INTO CARACTERISTIQUES VALUES ('2', 'couleur', 'noir');
+
+
+COMMIT;
