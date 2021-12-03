16e15d1549c2f6368fe7a0ccf23108841962704d
Valentin Guiziou <valentin.guiziou@grenoble-inp.org>
2021-12-03 16:25:20 +0000

    Replace Insert_tuple.sql

--- Insert_tuple.sql
+++ Insert_tuple.sql
@@ -1,12 +1,13 @@
-INSERT INTO PRODUITS VALUES ('1', 'XBOX 360', '250', 'console nulle', 'paspourlinstant', 'Console');
+INSERT INTO PRODUITS VALUES ('1', 'XBOX 360', '250', 'console nulle', 'https://www.cdiscount.com/pdt2/8/5/9/1/700x700/0882224035859/rw/console-xbox-360-premium.jpg',
+'Console');
 
-INSERT INTO PRODUITS VALUES ('2', 'Playstation 5', '300', 'trop bien', 'nop', 'Console');
+INSERT INTO PRODUITS VALUES ('2', 'Playstation 5', '300', 'trop bien', 'https://www.journaldugeek.com/content/uploads/2021/09/template-jdg-2021-09-14t154227-127.jpg', 'Console');
 
-INSERT INTO PRODUITS VALUES ('3', 'Guerre et Paix Partie 1', '12', 'Livre de Tolstoï, partie 1', 'plus tard', 'Roman');
+INSERT INTO PRODUITS VALUES ('3', 'Guerre et Paix Partie 1', '12', 'Livre de Tolstoï, partie 1', 'https://static.fnac-static.com/multimedia/Images/FR/NR/99/dc/03/253081/1540-1/tsp20211125070425/La-Guerre-et-la-Paix-La-Guerre-et-la-Paix-Tome-1.jpg', 'Roman');
 
-INSERT INTO PRODUITS VALUES ('4', 'Guerre et Paix Partie 2', '12', 'Livre de Tolstoï, partie 2', 'un jour peut être', 'Roman');
+INSERT INTO PRODUITS VALUES ('4', 'Guerre et Paix Partie 2', '12', 'Livre de Tolstoï, partie 2', 'https://static.fnac-static.com/multimedia/Images/FR/NR/13/81/14/1343763/1540-1/tsp20211127072831/La-Guerre-et-la-Paix.jpg', 'Roman');
 
-INSERT INTO PRODUITS VALUES ('5', 'Tintin au Congo', '15', 'Un peu border', 'si dieu le veut', 'BD');
+INSERT INTO PRODUITS VALUES ('5', 'Tintin au Congo', '15', 'Un peu border', 'https://static.fnac-static.com/multimedia/FR/Images_Produits/FR/fnac.com/Visual_Principal_340/5/1/0/9782203001015/tsp20120926112403/Tintin-au-Congo.jpg', 'BD');
 
 INSERT INTO CATEGORIES VALUES ('Livre');
 
