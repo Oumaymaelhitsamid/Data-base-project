4b484b1764940ac7185345c6746dbe7e5de043fd
Valentin Guiziou <valentin.guiziou@grenoble-inp.org>
2021-12-07 19:46:45 +0100

    Ajout 1 offre

--- src/query/InsertTuples.java
+++ src/query/InsertTuples.java
@@ -9,14 +9,14 @@ public class InsertTuples {
     static final String USER = "arvyp";
     static final String PASSWD = "arvyp";
 
-    private int N = 59;
+    private int N = 60;
     private static String[] insertions = new String[]{
             "INSERT INTO PRODUITS VALUES ('1', 'XBOX 360', '250', 'console sortie en 2005, vendu plus de 25 millions de fois en Europe', 'https://www.cdiscount.com/pdt2/8/5/9/1/700x700/0882224035859/rw/console-xbox-360-premium.jpg', 'Console')",
             "INSERT INTO PRODUITS VALUES ('2', 'Playstation 5', '300', 'console sortie en 2020 par Sony Interactive Entertainement', 'https://www.journaldugeek.com/content/uploads/2021/09/template-jdg-2021-09-14t154227-127.jpg', 'Console')",
             "INSERT INTO PRODUITS VALUES ('3', 'Guerre et Paix Partie 1', '12', 'Livre de Tolstoï, partie 1, sur la Russie pendant les guerres Napoleoniennes', 'https://static.fnac-static.com/multimedia/Images/FR/NR/99/dc/03/253081/1540-1/tsp20211125070425/La-Guerre-et-la-Paix-La-Guerre-et-la-Paix-Tome-1.jpg', 'Roman')",
             "INSERT INTO PRODUITS VALUES ('4', 'Guerre et Paix Partie 2', '12', 'Livre de Tolstoï, partie 2,sur la Russie pendant les guerres Napoleoniennes', 'https://static.fnac-static.com/multimedia/Images/FR/NR/13/81/14/1343763/1540-1/tsp20211127072831/La-Guerre-et-la-Paix.jpg', 'Roman')",
             "INSERT INTO PRODUITS VALUES ('5', 'Tintin au Congo', '15', 'Tintin voyage au Congo pour lutter contre des gangsters', 'https://static.fnac-static.com/multimedia/FR/Images_Produits/FR/fnac.com/Visual_Principal_340/5/1/0/9782203001015/tsp20120926112403/Tintin-au-Congo.jpg', 'BD')",
-            "INSERT INTO PRODUITS VALUES ('6', 'Peugeot 208', '10000', 'Peugeot occasion, en bonne état, 50 000km', 'https://lh3.googleusercontent.com/proxy/DX4b2G8nfGvU8GEUPs0OCke5iFACCdpci5Og-wF3gdcOkRHuNAECZJjkHgi63mRXYgpQYAgVxt0QNT_iHXm-yGbD5UAO3A4V33wUFVYxUcN4VNSd4WqSaywt', 'Voiture')",
+            "INSERT INTO PRODUITS VALUES ('6', 'Peugeot 208', '10400', 'Peugeot occasion, en bonne état, 50 000km', 'https://lh3.googleusercontent.com/proxy/DX4b2G8nfGvU8GEUPs0OCke5iFACCdpci5Og-wF3gdcOkRHuNAECZJjkHgi63mRXYgpQYAgVxt0QNT_iHXm-yGbD5UAO3A4V33wUFVYxUcN4VNSd4WqSaywt', 'Voiture')",
             "INSERT INTO PRODUITS VALUES ('7', 'Renault CLio 4', '11000', 'Clio occasion, en bonne état, 30 000km', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQi2KQfuis52hoVFykNZv5nFfrm4hYa_aVeA&usqp=CAU', 'Voiture')",
             "INSERT INTO PRODUITS VALUES ('8', 'Bugatti veyron', '2500000', 'Occasion en parfaite état, 13000km', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7tffHD0biW0BKbiEZw_P3Xl-Wcy3EKlssOg&usqp=CAU', 'Bugatti')",
            
@@ -79,8 +79,9 @@ public class InsertTuples {
             "INSERT INTO OFFRES VALUES ('6', '05/12/2021', '15:34:45.087845637', '10100', '1')",
             "INSERT INTO OFFRES VALUES ('6', '05/12/2021', '20:56:12.015841236', '10250', '2')",
             "INSERT INTO OFFRES VALUES ('6', '06/12/2021', '09:18:05.120474478', '10325', '3')",
-            "INSERT INTO OFFRES VALUES ('6', '06/12/2021', '17:05:24.987421214', '10400', '2')"
 
+            "INSERT INTO OFFRES VALUES ('6', '06/12/2021', '17:05:24.987421214', '10400', '2')",
+            "INSERT INTO OFFRES VALUES('1','04/12/2021','07:45:32.214563987','250','4')"
     };
 
     public InsertTuples() {
